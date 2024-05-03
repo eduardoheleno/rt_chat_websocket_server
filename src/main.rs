8b1523f21f7246c5bc2d4a203c58b3a0a05c9f05
Eduardo Gomes Heleno <eduardo.gheleno@gmail.com>
2024-05-02 23:27:36 -0300

    implements hashmap instead Vec and close websocket connection

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::{
-    net::{
+    collections::HashMap, net::{
         SocketAddr,
         TcpStream
     }, process::exit, sync::{
@@ -15,12 +15,11 @@ struct UserInfo {
     sender: Writer<TcpStream>
 }
 
-type UserPool = Arc<Mutex<Vec<UserInfo>>>;
+type UserPool = Arc<Mutex<HashMap<SocketAddr, UserInfo>>>;
 
 fn main() {
 	let server = Server::bind("127.0.0.1:8081").unwrap();
-
-    let user_pool: UserPool = Arc::new(Mutex::new(Vec::new()));
+    let user_pool: UserPool = Arc::new(Mutex::new(HashMap::new()));
 
 	for request in server.filter_map(Result::ok) {
         let up_ref = user_pool.clone();
@@ -41,20 +40,19 @@ fn main() {
             let client = request.accept().unwrap();
 			let ip = client.peer_addr().unwrap();
 
-			let (mut receiver, mut sender) = client.split().unwrap();
+			let (mut receiver, sender) = client.split().unwrap();
 
             let user_info = UserInfo { username: username_string.to_owned(), ip, sender };
 			println!("Connection from {}", user_info.ip);
 
             let mut locked_user_pool = up_ref.lock().unwrap();
-            locked_user_pool.push(user_info);
+            locked_user_pool.insert(ip, user_info);
             drop(locked_user_pool);
 
 			for message in receiver.incoming_messages() {
                 let message = message.expect("Error on fetching message");
 
                 match message {
-                    // only type supported for now
                     OwnedMessage::Text(m) => {
                         println!("message: {m}");
                         let message_with_username = format!("{};{}", m, username_string);
@@ -62,14 +60,22 @@ fn main() {
 
                         let mut locked_user_pool = up_ref.lock().unwrap();
                         locked_user_pool.iter_mut().for_each(|user_info| {
-                            user_info.sender.send_message(&to_send_message).expect("Could not send dataframe");
+                            user_info.1.sender.send_message(&to_send_message).expect("Could not send dataframe");
                         });
 
                         drop(locked_user_pool);
                     }
+                    OwnedMessage::Close(_) => {
+                        let mut locked_user_pool = up_ref.lock().unwrap();
+                        locked_user_pool.remove(&ip);
+                        drop(locked_user_pool);
+
+                        println!("closed connection from {ip}");
+                        return;
+                    }
                     _ => {
                         eprintln!("unsupported type of message");
-                        exit(2);
+                        exit(1);
                     }
                 }
 			}
